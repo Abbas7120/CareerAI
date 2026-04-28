6cb33a7642afb3e62712a53c03d923eecb049dc4
Abbas <abbasfzd118@gmail.com>
2026-04-29 02:48:12 +0530

    uploads

--- Saas-App-backend/routes/image.routes.js
+++ Saas-App-backend/routes/image.routes.js
@@ -1,16 +1,18 @@
 const express=require('express')
 const router=express.Router()
 const multer = require("multer");
-const upload = multer({
-  storage: multer.memoryStorage(),
-  limits: { fileSize: 10 * 1024 * 1024 },
-  fileFilter: (req, file, cb) => {
-    if (!file.mimetype.startsWith("image/")) {
-      return cb(new Error("Only image files are accepted"), false);
-    }
-    cb(null, true);
-  },
-});
+// const upload = multer({
+//   storage: multer.memoryStorage(),
+//   limits: { fileSize: 10 * 1024 * 1024 },
+//   fileFilter: (req, file, cb) => {
+//     if (!file.mimetype.startsWith("image/")) {
+//       return cb(new Error("Only image files are accepted"), false);
+//     }
+//     cb(null, true);
+//   },
+// });
+
+const upload=();
 
 const {removeBackground}=require("../controllers/image.controller.js")
 const {headshotGenerator}=require("../controllers/image.controller.js")
