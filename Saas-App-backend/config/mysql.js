3a93c0b6d2ffce77a7fa9ff13564425e2d1e8721
Abbas <abbasfzd118@gmail.com>
2026-05-31 16:34:23 +0530

    Configured TiDB

--- Saas-App-backend/config/mysql.js
+++ Saas-App-backend/config/mysql.js
@@ -1,12 +1,32 @@
-const mysql = require("mysql2/promise")
+// const mysql = require("mysql2/promise")
+
+// const pool = mysql.createPool({
+//  host: process.env.DB_HOST,
+//  user: process.env.DB_USER,
+//  password: process.env.DB_PASSWORD,
+//  database: process.env.DB_NAME,
+//   port: process.env.DB_PORT, 
+//  connectionLimit: 10
+// })
+
+// module.exports = pool
+
+const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
- host: process.env.DB_HOST,
- user: process.env.DB_USER,
- password: process.env.DB_PASSWORD,
- database: process.env.DB_NAME,
-  port: process.env.DB_PORT, 
- connectionLimit: 10
-})
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 
-module.exports = pool
+  ssl: {
+    minVersion: "TLSv1.2",
+    rejectUnauthorized: true,
+  },
+
+  waitForConnections: true,
+  connectionLimit: 10,
+});
+
+module.exports = pool;
